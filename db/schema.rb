b3c2def7b65e54e588dc9e5f5830b8b6a0aa41fe
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-09-22 10:15:58 -0400

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,106 +11,121 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120622043849) do
+ActiveRecord::Schema.define(:version => 20120720140817) do
 
-  create_table 'rails_admin_histories', :force => true do |t|
-    t.string   'message'
-    t.string   'username'
-    t.integer  'item'
-    t.string   'table'
-    t.integer  'month',      :limit => 2
-    t.integer  'year',       :limit => 8
-    t.datetime 'created_at',              :null => false
-    t.datetime 'updated_at',              :null => false
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
-  add_index 'rails_admin_histories', ['item', 'table', 'month', 'year'], :name => 'index_rails_admin_histories'
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
-  create_table 'referees', :force => true do |t|
-    t.string   'endpoint'
-    t.datetime 'created_at', :null => false
-    t.datetime 'updated_at', :null => false
+  create_table "rails_admin_histories", :force => true do |t|
+    t.string   "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
   end
 
-  create_table 'referrals', :force => true do |t|
-    t.integer  'referee_id'
-    t.integer  'user_id'
-    t.datetime 'created_at',                :null => false
-    t.datetime 'updated_at',                :null => false
-    t.integer  'visits',     :default => 0
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
+
+  create_table "referees", :force => true do |t|
+    t.string   "endpoint"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
-  create_table 'reminders', :force => true do |t|
-    t.datetime 'created_at',                      :null => false
-    t.datetime 'updated_at',                      :null => false
-    t.integer  'from_user_id',                    :null => false
-    t.integer  'to_user_id',                      :null => false
-    t.integer  'thing_id',                        :null => false
-    t.boolean  'sent',         :default => false
+  create_table "referrals", :force => true do |t|
+    t.integer  "referee_id"
+    t.integer  "user_id"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.integer  "visits",     :default => 0
   end
 
-  add_index 'reminders', ['from_user_id'], :name => 'index_reminders_on_from_user_id'
-  add_index 'reminders', ['sent'], :name => 'index_reminders_on_sent'
-  add_index 'reminders', ['thing_id'], :name => 'index_reminders_on_thing_id'
-  add_index 'reminders', ['to_user_id'], :name => 'index_reminders_on_to_user_id'
-
-  create_table 'sessions', :id => false, :force => true do |t|
-    t.string   'session_id', :null => false
-    t.text     'data'
-    t.datetime 'created_at', :null => false
-    t.datetime 'updated_at', :null => false
+  create_table "reminders", :force => true do |t|
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+    t.integer  "from_user_id",                    :null => false
+    t.integer  "to_user_id",                      :null => false
+    t.integer  "thing_id",                        :null => false
+    t.boolean  "sent",         :default => false
   end
 
-  add_index 'sessions', ['session_id'], :name => 'index_sessions_on_session_id'
-  add_index 'sessions', ['updated_at'], :name => 'index_sessions_on_updated_at'
+  add_index "reminders", ["from_user_id"], :name => "index_reminders_on_from_user_id"
+  add_index "reminders", ["sent"], :name => "index_reminders_on_sent"
+  add_index "reminders", ["thing_id"], :name => "index_reminders_on_thing_id"
+  add_index "reminders", ["to_user_id"], :name => "index_reminders_on_to_user_id"
 
-  create_table 'things', :force => true do |t|
-    t.datetime 'created_at',                                                             :null => false
-    t.datetime 'updated_at',                                                             :null => false
-    t.string   'name'
-    t.decimal  'lat',                 :precision => 18, :scale => 14,                    :null => false
-    t.decimal  'lng',                 :precision => 18, :scale => 14,                    :null => false
-    t.integer  'user_id'
-    t.decimal  'snow_cover',          :precision => 5,  :scale => 2,  :default => 0.0
-    t.integer  'lock_version',                                        :default => 0,     :null => false
-    t.boolean  'sms_notifications',                                   :default => false
-    t.boolean  'email_notifications',                                 :default => false
+  create_table "sessions", :id => false, :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
-  create_table 'users', :force => true do |t|
-    t.datetime 'created_at',                                               :null => false
-    t.datetime 'updated_at',                                               :null => false
-    t.string   'name',                                                     :null => false
-    t.string   'organization'
-    t.string   'email',                                                    :null => false
-    t.string   'sms_number'
-    t.boolean  'admin',                                 :default => false
-    t.string   'encrypted_password',     :limit => 128, :default => '',    :null => false
-    t.string   'reset_password_token'
-    t.string   'remember_token'
-    t.datetime 'remember_created_at'
-    t.integer  'sign_in_count',                         :default => 0
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string   'current_sign_in_ip'
-    t.string   'last_sign_in_ip'
-    t.string   'address_1'
-    t.string   'address_2'
-    t.string   'city'
-    t.string   'state'
-    t.string   'zip'
-    t.datetime 'reset_password_sent_at'
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "things", :force => true do |t|
+    t.datetime "created_at",                                                             :null => false
+    t.datetime "updated_at",                                                             :null => false
+    t.string   "name"
+    t.decimal  "lat",                 :precision => 18, :scale => 14,                    :null => false
+    t.decimal  "lng",                 :precision => 18, :scale => 14,                    :null => false
+    t.integer  "user_id"
+    t.decimal  "snow_cover",          :precision => 5,  :scale => 2,  :default => 0.0
+    t.integer  "lock_version",                                        :default => 0,     :null => false
+    t.boolean  "sms_notifications",                                   :default => false
+    t.boolean  "email_notifications",                                 :default => false
   end
 
-  add_index 'users', ['email'], :name => 'index_users_on_email', :unique => true
+  create_table "users", :force => true do |t|
+    t.datetime "created_at",                                               :null => false
+    t.datetime "updated_at",                                               :null => false
+    t.string   "name",                                                     :null => false
+    t.string   "organization"
+    t.string   "sms_number"
+    t.boolean  "admin",                                 :default => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "address_1"
+    t.string   "address_2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.datetime "reset_password_sent_at"
+    t.integer  "facebook_id",            :limit => 8
+    t.string   "email"
+  end
 
-  add_foreign_key 'referrals', 'referees', :name => 'referrals_referee_id_fk'
-  add_foreign_key 'referrals', 'users', :name => 'referrals_user_id_fk'
+  add_foreign_key "referrals", "referees", :name => "referrals_referee_id_fk"
+  add_foreign_key "referrals", "users", :name => "referrals_user_id_fk"
 
-  add_foreign_key 'reminders', 'things', :name => 'reminders_thing_id_fk'
-  add_foreign_key 'reminders', 'users', :name => 'reminders_from_user_id_fk', :column => 'from_user_id'
-  add_foreign_key 'reminders', 'users', :name => 'reminders_to_user_id_fk', :column => 'to_user_id'
+  add_foreign_key "reminders", "things", :name => "reminders_thing_id_fk"
+  add_foreign_key "reminders", "users", :name => "reminders_from_user_id_fk", :column => "from_user_id"
+  add_foreign_key "reminders", "users", :name => "reminders_to_user_id_fk", :column => "to_user_id"
 
-  add_foreign_key 'things', 'users', :name => 'things_user_id_fk'
+  add_foreign_key "things", "users", :name => "things_user_id_fk"
 
 end
