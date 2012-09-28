3b79d73822c3f493822852d4de7142c532d18982
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-09-28 05:20:06 -0400

    update

--- app/mailers/thing_mailer.rb
+++ app/mailers/thing_mailer.rb
@@ -1,6 +1,6 @@
 # http://stackoverflow.com/questions/6021372/best-way-to-create-unique-token-in-rails
 class ThingMailer < ActionMailer::Base
-  default :from => 'adoptahydrantsyracuse@gmail.com'
+  default :from => 'adoptahydrantprovidence@gmail.com'
 
   def reminder(thing)
     @thing = thing
@@ -10,6 +10,6 @@ class ThingMailer < ActionMailer::Base
   end
   
   def notify(thing)
-    mail(:to => thing.user.email, :from => 'adoptahydrantsyracuse@gmail.com', :subject => thing.name, :body => thing.user.name + ', ' + thing.name + ' might be surrounded by ' + thing.snow_cover.to_s + ' in. of snow. Location: ' + thing.full_address + '.')
+    mail(:to => thing.user.email, :from => 'adoptahydrantprovidence@gmail.com', :subject => thing.name, :body => thing.user.name + ', ' + thing.name + ' might be surrounded by ' + thing.snow_cover.to_s + ' in. of snow. Location: ' + thing.full_address + '.')
   end
 end
